79bef06e2115072852145d6ad8421a46c035a01c
David Rawson <david.rawson@trademe.co.nz>
2020-04-15 17:53:56 +1200

    Add Retrofit service

---
+++ app/src/main/java/com/tsongkha/random/base/network/RandomUserService.kt
@@ -0,0 +1,17 @@
+package com.tsongkha.random.base.network
+
+import com.tsongkha.random.user.Result
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RandomUserService {
+
+    @GET("/api/")
+    suspend fun users(
+        @Query("page") page: Int,
+        @Query("seed") seed: String,
+        @Query("results") results: Int,
+        @Query("exc") exclude: String?,
+        @Query("inc") include: String?
+    ): Result
+}
