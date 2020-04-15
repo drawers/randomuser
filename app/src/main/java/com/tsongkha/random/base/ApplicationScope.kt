92dd809ad0ba965f4af828453344dacdb52e6e92
David Rawson <david.rawson@trademe.co.nz>
2020-04-15 15:34:06 +1200

    Add base application

---
+++ app/src/main/java/com/tsongkha/random/base/ApplicationScope.kt
@@ -0,0 +1,5 @@
+package com.tsongkha.random.base
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationScope
---
+++ app/src/main/java/com/tsongkha/random/base/RandomUserApplication.kt
@@ -0,0 +1,23 @@
+package com.tsongkha.random.base
+
+import android.app.Application
+import toothpick.Scope
+import toothpick.ktp.KTP
+import toothpick.ktp.binding.bind
+import toothpick.ktp.binding.module
+
+class RandomUserApplication : Application() {
+
+    lateinit var scope: Scope
+
+    override fun onCreate() {
+        super.onCreate()
+
+        scope = KTP.openScope(ApplicationScope::class.java)
+            .installModules(
+                module {
+                    bind<Application>().toInstance { this@RandomUserApplication }
+                }
+            )
+    }
+}
