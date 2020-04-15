1977ad9a880c12aea92040593eebd016ee4419c6
David Rawson <david.rawson@trademe.co.nz>
2020-04-15 18:31:45 +1200

    Move provider to separate file

--- app/src/main/java/com/tsongkha/random/base/network/NetworkModule.kt
+++ app/src/main/java/com/tsongkha/random/base/network/NetworkModule.kt
@@ -2,10 +2,8 @@ package com.tsongkha.random.base.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import toothpick.InjectConstructor
 import toothpick.ktp.binding.bind
 import toothpick.ktp.binding.module
-import javax.inject.Provider
 
 val networkModule = module {
     bind<Retrofit>().toInstance {
@@ -15,12 +13,4 @@ val networkModule = module {
             .build()
     }
     bind<UserService>().toProvider(UserServiceProvider::class)
-}
-
-@InjectConstructor
-class UserServiceProvider(private val retrofit: Retrofit) : Provider<UserService> {
-
-    override fun get(): UserService {
-        return retrofit.create(UserService::class.java)
-    }
 }
