2abfa51f554a01ca72def8f269d5d397699024db
David Rawson <david.rawson@trademe.co.nz>
2020-04-17 07:36:04 +1200

    Add placeholder epoxy model

---
+++ app/src/main/java/com/tsongkha/random/list/presentation/UserPlaceholderEpoxyModel.kt
@@ -0,0 +1,9 @@
+package com.tsongkha.random.list.presentation
+
+import android.view.View
+import com.airbnb.epoxy.EpoxyModel
+import com.airbnb.epoxy.EpoxyModelClass
+import com.tsongkha.random.R
+
+@EpoxyModelClass(layout = R.layout.item_user_placeholder)
+abstract class UserPlaceholderEpoxyModel : EpoxyModel<View>()
