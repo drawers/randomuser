279e7c6d659a64fff636821d9b135304ded7fe6b
David Rawson <david.rawson@trademe.co.nz>
2020-04-16 20:20:13 +1200

    Add circle crop transformation

--- app/src/main/java/com/tsongkha/random/list/presentation/UserEpoxyModel.kt
+++ app/src/main/java/com/tsongkha/random/list/presentation/UserEpoxyModel.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import coil.api.load
+import coil.transform.CircleCropTransformation
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
@@ -32,7 +33,9 @@ abstract class UserEpoxyModel : EpoxyModelWithHolder<UserEpoxyModel.Holder>() {
             findViewById<TextView>(R.id.nameTextView).text = titleName
             findViewById<TextView>(R.id.genderTextView).text = gender
             findViewById<TextView>(R.id.dobTextView).text = dob
-            findViewById<ImageView>(R.id.thumbnailImageView).load(uri = thumbnail)
+            findViewById<ImageView>(R.id.thumbnailImageView).load(uri = thumbnail) {
+                transformations(CircleCropTransformation())
+            }
         }
     }
 
