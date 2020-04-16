8dd1d089dcac18d5c596759eea3abb7a91faa39a
David Rawson <david.rawson@trademe.co.nz>
2020-04-17 08:09:51 +1200

    Add detail

---
+++ app/src/main/java/com/tsongkha/random/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.tsongkha.random.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tsongkha.random.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
